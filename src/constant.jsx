e141828c8fa2ee997818b2bfc7ee341b5ad230e0
Shivam seth <shivamseth221104@gmail.com>
2023-07-10 23:03:24 +0530

    okk

--- src/constant.jsx
+++ src/constant.jsx
@@ -1,2 +1,4 @@
-export const tumb_img ="https://media.licdn.com/dms/image/D4D16AQGBu4rPhbqO8w/profile-displaybackgroundimage-shrink_200_800/0/1672152509446?e=1694044800&v=beta&t=K7mvRJQxi3lKT37MGFtehgXdBvMP3Nnt9LpGCnLTuOQ";
-export const cir_img ="https://media.licdn.com/dms/image/D4D35AQETQekL_lrbKg/profile-framedphoto-shrink_400_400/0/1687772942557?e=1689019200&v=beta&t=iZNnXuAsNgdvP6Uhj0yJyyWjKhxUuG4WVBg-XIJuFE4";
+export const tumb_img =
+  "https://media.licdn.com/dms/image/D4D16AQGBu4rPhbqO8w/profile-displaybackgroundimage-shrink_200_800/0/1672152509446?e=1694044800&v=beta&t=K7mvRJQxi3lKT37MGFtehgXdBvMP3Nnt9LpGCnLTuOQ";
+export const cir_img =
+  "https://media.licdn.com/dms/image/D4D35AQETQekL_lrbKg/profile-framedphoto-shrink_400_400/0/1687772942557?e=1689019200&v=beta&t=iZNnXuAsNgdvP6Uhj0yJyyWjKhxUuG4WVBg-XIJuFE4";
